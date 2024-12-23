4ffb93dedca2fb43e0a398af727fcc1296fd23b7
marshall-bits <kz_@hotmail.es>
2024-12-23 13:01:34 +0100

    little refactor

--- src/script.js
+++ src/script.js
@@ -3,10 +3,8 @@ const navMenu = document.querySelector("#nav-menu");
 const menuListItems = document.querySelectorAll("#nav-menu li");
 
 menuButton.addEventListener("click", () => {
-  navMenu.classList.remove("animate-slide-out");
-  navMenu.classList.toggle("hidden");
-  navMenu.classList.add("flex");
-  navMenu.classList.add("animate-slide-in");
+  navMenu.classList.remove("animate-slide-out", "hidden");
+  navMenu.classList.add("flex", "animate-slide-in");
 });
 
 menuListItems.forEach((item) => {
@@ -15,18 +13,18 @@ menuListItems.forEach((item) => {
     if (windowWidth > 640) return;
     navMenu.classList.remove("animate-slide-in");
     navMenu.classList.add("animate-slide-out");
-
-    setTimeout(() => {
-      navMenu.classList.toggle("hidden");
-      navMenu.classList.remove("flex");
-    }, 400);
   });
+});
+
+navMenu.addEventListener("animationend", (e) => {
+  if (e.animationName !== "slide-out") return;
+  navMenu.classList.add("hidden");
+  navMenu.classList.remove("flex");
 });
 
 window.addEventListener("resize", () => {
   const windowWidth = window.innerWidth;
   if (windowWidth > 640) {
-    navMenu.classList.remove("animate-slide-in");
-    navMenu.classList.remove("animate-slide-out");
+    navMenu.classList.remove("animate-slide-in", "animate-slide-out");
   }
 });
